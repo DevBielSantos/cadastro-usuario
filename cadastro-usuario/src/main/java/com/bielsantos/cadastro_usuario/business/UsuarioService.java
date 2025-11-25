d778496331fd299799f8fe9a0d2f9c03a93eece7
DevBielSantos <devbielst@gmail.com>
2025-11-25 16:02:28 -0300

    Add atualizarUsuarioPorId

--- cadastro-usuario/src/main/java/com/bielsantos/cadastro_usuario/business/UsuarioService.java
+++ cadastro-usuario/src/main/java/com/bielsantos/cadastro_usuario/business/UsuarioService.java
@@ -31,8 +31,18 @@ public class UsuarioService {
     }
 
 
-    public void atualizarUsuarioPorEmail(String email, Usuario usuario){
-        Usuario usuarioEntity = buscarUsuarioPorEmail(email);
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario){
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() ->
+            new RuntimeException("Usuario não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() :
+                        usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() :
+                        usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+
+        repository.saveAndFlush(usuarioAtualizado);
 
 
     }
