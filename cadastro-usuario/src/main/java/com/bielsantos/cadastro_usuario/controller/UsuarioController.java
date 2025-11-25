a662923305d29bec50763ba6c55df6cbddc7edd0
DevBielSantos <devbielst@gmail.com>
2025-11-25 16:58:25 -0300

    Repair @PostMapping

--- cadastro-usuario/src/main/java/com/bielsantos/cadastro_usuario/controller/UsuarioController.java
+++ cadastro-usuario/src/main/java/com/bielsantos/cadastro_usuario/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
 
-    @PatchMapping
+    @PostMapping
     public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
         usuarioService.salvarUsuario(usuario);
         return ResponseEntity.ok().build();
